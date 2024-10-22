d580ca7740c112e115bb5d90873ef2610e0984fd
NasraMohammad <nasra.m@sinc.business>
2024-10-22 12:55:56 +0300

    Replace apple and Common folder with content of v3.8.1

--- Common/cpp/hidden_headers/Logger.h
+++ Common/cpp/hidden_headers/Logger.h
@@ -1,22 +1,24 @@
 #pragma once
 
-#include "./LoggerInterface.h"
 #include <memory>
+#include <stdexcept>
 
-namespace reanimated
-{
+#include "./LoggerInterface.h"
+
+namespace reanimated {
 
 class Logger {
-  public:
-  template<typename T>
-    static void log(T value) {
-      if (instance == nullptr) {
-        throw std::runtime_error("no logger specified");
-      }
-      instance->log(value);
-    };
-  private:
-    static std::unique_ptr<LoggerInterface> instance;
+ public:
+  template <typename T>
+  static void log(T value) {
+    if (instance == nullptr) {
+      throw std::runtime_error("[Reanimated] No logger specified.");
+    }
+    instance->log(value);
+  }
+
+ private:
+  static std::unique_ptr<LoggerInterface> instance;
 };
 
-}
+} // namespace reanimated
